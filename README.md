c4e222bd150b53ac4e0bf68f8c913bdb68fd506a
liujian10 <liujian10@le.com>
2017-12-14 18:16:01 +0800

    add code

--- README.md
+++ README.md
@@ -1,2 +1,57 @@
 # marquee-js
 Js marquee plugin with CSS3 support
+
+![demo](assets/sheet.jpg)
+
+## Install
+
+[![marquee-js](https://nodei.co/npm/marquee-js.png)](https://npmjs.org/package/marquee-js)
+
+## Development
+
+```bash
+# install
+npm install --save marquee-js
+
+# run
+gulp server
+
+# build
+gulp build
+```
+
+## Example
+
+local: http://localhost:3001/
+
+online: http://mapleliu.com/v-drawer/
+
+## Usage
+
+用于处理文字，超长自动循环滚动；
+
+```html
+<script src='marquee.js'></script>
+```
+
+```
+marqueeJs(dom,options);
+```
+
+## API
+
+### props
+
+| 参数 | 说明 | 类型 | 默认值 | 单位 |
+|-----------|-----------|-----------|-------------|-------------|
+| dom | 需要处理的dom元素 | `HTMLElement` | - | - |
+| options | 配置项 | `Object` | - | - |
+
+### options
+
+| 参数 | 说明 | 类型 | 默认值 | 单位 |
+|-----------|-----------|-----------|-------------|-------------|
+| speed | 更新速度 | `Number` | - | `ms(毫秒)/px(象素)` |
+| width | 自定义元素宽度 | `Number` | 元素自身宽度 | `px` |
+| height | 自定义元素高度 | `Number` | 元素自身高度 | `px` |
+| delay | 延时时长 | `Number` | 0 | `ms` |
--- marquee.js
+++ marquee.js
@@ -1,68 +1,4 @@
-(function($, win) {
-  /*文案过长时以跑马灯效果展示*/
-  $.prototype.marqueeCss = function(data) {
-    var speed = data && data.speed || 10;
-    var dom = $(this);
-    var inHtml = dom.html();
-    var inWidth = data && data.width || dom.outerWidth();
-    dom.attr('style', 'position: fixed;display:block;width:auto;word-break: keep-all;white-space: nowrap;');
-    var inHeight = data && data.height || dom.innerHeight(), outWidth = dom.outerWidth();
-    if (outWidth >= inWidth) {
-      dom.attr('style', 'position:relative;display:inline-block;overflow: hidden;width:' + inWidth + 'px;height:' +
-          inHeight + 'px').html('<div class=\'marquee\'>' + inHtml + '</div>');
-      var marqueeStyle = '.marquee{position:absolute;left:' + -1 * outWidth + 'px;display:inline-block;width:' +
-          outWidth +
-          'px;padding:0;overflow:hidden;-webkit-animation:marquee-animate 10s linear 0s infinite;animation:marquee-animate ' +
-          speed + 's linear 0s infinite}' +
-          '@-webkit-keyframes marquee-animate{0%{left:100%}100%{left:' + -1 * outWidth + 'px}}' +
-          '@keyframes marquee-animate{0%{left:100%}100%{left:' + -1 * outWidth + 'px}}';
-      if ($('#marquee-style').html()) {
-        $('#marquee-style').html(marqueeStyle);
-      } else {
-        $('body').append('<style id=\'marquee-style\'>' + marqueeStyle + '</style>');
-      }
-    } else {
-      dom.attr('style', '');
-    }
-  };
-
-  $.prototype.marqueeJs = function(data) {
-    var speed = data && data.speed || 100,
-        dom = $(this),
-        inHtml = dom.html(),
-        inWidth = data && data.width || dom.outerWidth(),
-        itemMargin = 50,
-        delay = data.delay ? data.delay : 0;
-    dom.attr('style', 'position: fixed;display:block;width:auto;word-break: keep-all;white-space: nowrap;');
-    var inHeight = data && data.height || dom.innerHeight(), outWidth = dom.outerWidth();
-    if (outWidth >= inWidth) {
-      var itemWidth = outWidth + itemMargin;
-      dom.attr('style', 'position: relative;display:inline-block;overflow:hidden;width:' + inWidth + 'px;height:' +
-          inHeight + 'px');
-      dom.html('<span style=\'position:absolute;top:0;left:' + inWidth + 'px;width:' + itemWidth + 'px;\'>' + inHtml +
-          '</span>' +
-          '<span style=\'position:absolute;top:0;left:' + (inWidth + itemWidth) + 'px;width:' + itemWidth + 'px;\'>' +
-          inHtml + '</span>');
-      var leftItem = dom.find('span')[0],
-          rightItem = dom.find('span')[1];
-
-      function marqueeToDo() {
-        leftItem.style.left = (parseInt(leftItem.style.left) - 1) + 'px';
-        rightItem.style.left = (parseInt(rightItem.style.left) - 1) + 'px';
-        if (parseInt(leftItem.style.left) <= -1 * itemWidth) {
-          leftItem.style.left = '0px';
-          rightItem.style.left = itemWidth + 'px';
-        }
-      }
-
-      setTimeout(function() {
-        setInterval(marqueeToDo, speed);
-      }, delay);
-    } else {
-      dom.attr('style', '');
-    }
-  };
-
+(function(win) {
   var isHTMLElement = ( typeof HTMLElement === 'object' ) ?
       function(obj) {
         return obj instanceof HTMLElement;
@@ -78,7 +14,7 @@
         inWidth = options && options.width || dom.offsetWidth,
         itemMargin = 50,
         delay = options.delay ? options.delay : 0;
-    var domStyle = dom.hasAttribute('style')?dom.getAttribute('style'):'';
+    var domStyle = dom.hasAttribute('style') ? dom.getAttribute('style') : '';
     dom.setAttribute('style', 'position: fixed;display:block;width:auto;word-break: keep-all;white-space: nowrap;');
     var inHeight = options && options.height || dom.clientHeight,
         outWidth = dom.offsetWidth;
@@ -115,4 +51,4 @@
   };
 
   win.marqueeJs = marqueeJs;
-})(jQuery, window);
+})(window);
